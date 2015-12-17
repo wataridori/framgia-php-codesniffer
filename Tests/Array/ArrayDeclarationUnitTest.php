60a72658c3b39dd27d0206a192fe41c293d1d397
Tran Duc Thang <thangtd90@gmail.com>
2015-12-17 10:46:46 +0700

    Add array declaration sniff

---
+++ Tests/Array/ArrayDeclarationUnitTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Unit test class for the ArrayDeclaration sniff.
+ */
+
+class Framgia_Tests_Array_ArrayDeclarationUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+            3 => 1,
+            5 => 1,
+            6 => 2,
+            7 => 1,
+            8 => 2,
+            10 => 2,
+            12 => 2,
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+}//end class
